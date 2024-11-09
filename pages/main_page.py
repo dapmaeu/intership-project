3276048b37518c61f42a9dd49b9d490e8efdfd2a
Maria D <dapellome@gmail.com>
2024-11-08 18:31:23 -0800

    Task 3

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,18 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
 from pages.base_page import Page
 
 class MainPage(Page):
     #URL = 'https://soft.reelly.io'
-    SECONDARY_BTN = (By.XPATH, "//a[text()='Secondary']")
-
+    SECONDARY_BTN = (By.CSS_SELECTOR, "a[href='/secondary-listings']")
+    EMAIL_FIELD = (By.CSS_SELECTOR, "#email-2")
 
     def open_main_page(self):
         self.open('https://soft.reelly.io')
+        sleep(3)
 
     def click_secondary_button(self):
         self.click(*self.SECONDARY_BTN)
--- pages/sign_in_page.py
+++ pages/sign_in_page.py
@@ -1,29 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 from pages.base_page import Page
 
 class SignInPage(Page):
-    SIGN_IN_BTN = (By.XPATH, "//a[text()='Continue']")
+    SIGN_IN_BTN = (By.CSS_SELECTOR, "a[wized='loginButton']")
     EMAIL_FIELD = (By.CSS_SELECTOR, "#email-2")
     EMAIL = 'dapellome@gmail.com'
     PASSWORD_FIELD = (By.CSS_SELECTOR, "#field")
     PASSWORD = 'Renderos13!'
+    SECONDARY_BTN = (By.CSS_SELECTOR, "a[href='/secondary-listings']")
 
-
-    def email_address(self, email):
-        self.input_text(self.EMAIL, *self.EMAIL_FIELD)
-
-    def input_password(self, password):
-        self.input_text(self.PASSWORD, *self.PASSWORD_FIELD)
-
-    def click_sign_in_button(self):
-        self.click(*self.SIGN_IN_BTN)
 
     def login(self):
         self.input_text(self.EMAIL, *self.EMAIL_FIELD)
+        sleep(3)
         self.input_text(self.PASSWORD, *self.PASSWORD_FIELD)
+        self.wait_to_be_clickable_click(*self.SIGN_IN_BTN)
         self.click(*self.SIGN_IN_BTN)
+
+
 
 
